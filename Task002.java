f5b0e8885fec4d30d049d0763d95d2b9ad8b2f93
Alina Smaliar <alina_smaliar@epam.com>
2020-02-17 15:11:58 +0300

    Task 002

---
+++ Task002.java
@@ -0,0 +1,30 @@
+package by.htp.unit02.main;
+
+public class Task002 {
+	public static void main(String[]args) {
+		
+		for (int i = 1; i < 6; i++){
+			System.out.println("i = "+i);
+		}
+		
+		System.out.println("\n");
+		
+		for (int j = 5; j > 0; j = j - 1){
+			System.out.println("i = "+j);
+		}
+		System.out.println("\n");
+		
+		int temp = 1;
+		
+		for (int z = 1; z < 11; z++)
+		{
+			temp = z *3;
+			System.out.println("3 * "+z+ " = " + temp);
+		}
+		
+		
+	 }
+}
+
+
+
